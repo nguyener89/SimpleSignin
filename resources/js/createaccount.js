def53e93708ff2892ab32a5cfcf98d6ff9cc742b
michael <nguyener89@gmail.com>
2017-09-02 15:58:12 -0500

    Confirm password feature added

--- resources/js/createaccount.js
+++ resources/js/createaccount.js
@@ -1,1 +1,15 @@
 'use strict';
+
+var password = document.getElementById("password"),
+    confirmpassword = document.getElementById("confirmpassword");
+
+function validatePassword() {
+    if(password.value != confirmpassword.value) {
+        confirmpassword.setCustomValidity("Passwords don't match");
+    } else {
+        confirmpassword.setCustomValidity('');
+    }
+}
+
+password.onchange = validatePassword();
+confirmpassword.onkeyup = validatePassword();
